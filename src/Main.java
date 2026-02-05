e54e17ccd51848c38b1a7be333e1cc01fafc7070
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-02-05 23:36:16 +0530

    added money withdrawal  options

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ class homepage{
     void homepage(){
         System.out.println("Enter Your Choice");
         System.out.println("1)Deposit  Money ");
+        System.out.println("2)Withdraw Money");
     }
 }
-class Main{
+class  data{
+   static String bankName;
+   static String accountHolderName;
+   static String accountType;
+   static long accountNumber;
+}
+class Main extends data{
     static void main(String[] args) {
         System.out.println("**********Wel-Come To Bank *************");
         Scanner input = new Scanner(System.in);
@@ -17,14 +24,28 @@ class Main{
                 System.out.println("Enter The Bank Name");
                 input.nextLine();
                 String bankName = input.nextLine();
+                System.out.println("Enter Account Holder Name");
+                accountHolderName = input.nextLine();
                 System.out.println("Enter Account Type ");
-                String accountType = input.nextLine();
+                accountType = input.nextLine();
                 System.out.println("Enter Account Number");
-                long accountNumber = input.nextLong();
+                 accountNumber = input.nextLong();
                 System.out.println("Enter The Amount");
                 int Amount = input.nextInt();
                 System.out.println("Money Deposited Successfully");
                 break;
+            case 2:
+                System.out.println("Enter Account Holder Name");
+                input.nextLine();
+                accountHolderName = input.nextLine();
+                System.out.println("Enter Account Type ");
+                accountType = input.nextLine();
+                System.out.println("Enter Account Number");
+                accountNumber = input.nextLong();
+                System.out.println("Enter The Amount");
+                int withdrawalAmount = input.nextInt();
+                System.out.println("Money Withdraw Successfully");
+
         }
     }
 }
