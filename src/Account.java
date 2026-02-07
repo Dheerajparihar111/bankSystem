3b6fb0715feadd7262eb5728d729053c85abe0a1
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-02-07 23:20:22 +0530

    added Account folder

---
+++ src/Account.java
@@ -0,0 +1,32 @@
+public class Account {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+    public Account(int accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = 0.0;
+}
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("₹" + amount + " deposited successfully.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("₹" + amount + " withdrawn successfully.");
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+    public void displayAccount() {
+        System.out.println("\n--- Account Details ---");
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolderName);
+        System.out.println("Balance        : ₹" + balance);
+    }
+}
